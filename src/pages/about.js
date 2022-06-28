19a19e299987b69d41b1a192a7e0aa2d763fb4f7
jinsu291 <wlstn291@naver.com>
2022-06-28 15:39:44 +0900

    Head 컴포넌트 도입

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,12 @@
-import { Link } from "gatsby";
 import React from "react";
+import Head from "../components/head";
 
 function AboutPage() {
     return (
     <main>
         <title>자기소개</title>
 
-        <div style = {{display:"flex", gap : 10 }}>
-            <Link to="/">홈</Link>
-            <Link to="/about">어바웃</Link>
-            <Link to="/test">테스트</Link>
-            <Link to="/test-space">테스트-스페이스</Link>
-        </div>
+        <Head />
 
         <h1>자기소개 페이지 입니다.</h1>
     </main>
--- src/pages/test-space.js
+++ src/pages/test-space.js
@@ -1,17 +1,12 @@
-import { Link } from "gatsby";
 import React from "react";
+import Head from "../components/head";
 
 function TestSpacePage() {
     return (
     <main>
         <title>테스트</title>
 
-        <div style = {{display:"flex", gap : 10 }}>
-            <Link to="/">홈</Link>
-            <Link to="/about">어바웃</Link>
-            <Link to="/test">테스트</Link>
-            <Link to="/test-space">테스트-스페이스</Link>
-        </div>
+        <Head />
 
         <h1>테스트 스페이스 페이지 입니다.</h1>
     </main>
